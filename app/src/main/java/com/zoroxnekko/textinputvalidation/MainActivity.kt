303b486a688c28bc20b89951e69ff2383b748c52
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-27 07:28:51 +0545

    add MainActivity.kt that calls the TextInputScreenRoot

--- app/src/main/java/com/zoroxnekko/textinputvalidation/MainActivity.kt
+++ app/src/main/java/com/zoroxnekko/textinputvalidation/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.zoroxnekko.textinputvalidation.text_input.TextInputScreenRoot
 import com.zoroxnekko.textinputvalidation.ui.theme.TextInputValidationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TextInputValidationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) { innerPadding ->
+                    TextInputScreenRoot(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TextInputValidationTheme {
-        Greeting("Android")
-    }
-}
